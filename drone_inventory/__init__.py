c172ea4f40ba7a867fb509c512bc55ee775b7306
Kevin Stevens <stevens.kevin.m@gmail.com>
2021-05-11 19:03:19 -0500

    drone_inventory hosting

---
+++ drone_inventory/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from config import Config
+from .site.routes import site
+from .authentication.routes import auth
+from .api.routes import api
+from flask_migrate import Migrate
+from drone_inventory.models import db as root_db, login_manager, ma
+from flask_sqlalchemy import SQLAlchemy
+
+
+# CORS - Cross Origin Resource Sharing
+from flask_cors import CORS
+
+from drone_inventory.helpers import JSONEncoder
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+app.register_blueprint(site)
+app.register_blueprint(auth)
+app.register_blueprint(api)
+
+root_db.init_app(app)
+migrate = Migrate(app, root_db)
+
+login_manager.init_app(app)
+ma.init_app(app)
+CORS(app)
+
+app.json_encoder = JSONEncoder
+
+# Specifying a route for non-authorized users
+login_manager.login_view = 'signin' 
+from drone_inventory import models
